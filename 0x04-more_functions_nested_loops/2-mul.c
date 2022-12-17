2913752037f2b67dd3752657d7118b098f93e23d
themrsgod <themrsgod@gmail.com>
2022-12-17 15:40:24 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multipy two numbers.
+ *
+ * @first: first number.
+ * @second: second number
+ * Return: result.
+ */
+int mul(int first, int second)
+{
+	int result;
+
+	result = first * second;
+	return (result);
+}

4c153bedc0c2463e062f5fb697b53cbd4df7eb3c
themrsgod <themrsgod@gmail.com>
2022-12-17 14:57:16 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - add two intergers.
+ * @first: first integer
+ * @second: second integer
+ * Return: Always 0.
+ */
+int add(int first, int second)
+{
+	int sum;
+
+	sum = first + second;
+	return (sum);
+}

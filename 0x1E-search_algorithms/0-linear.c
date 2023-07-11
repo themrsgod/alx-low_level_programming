495dab8090d987228833fa2f4f8e1efdf9ee395d
themrsgod <themrsgod@gmail.com>
2023-07-11 16:56:35 -0700

    function that searches

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,6 +2,10 @@
 
 /*
  * search_function - function that searches for a values
+ * @array: - is a pointer to the first element of the array to search in
+ * @size: - is the number of elements in array
+ * @value: - is the value to search for
+ * Return: - the first index where value is located
  */
 int linear_search(int *array, size_t size, int value)
 {
